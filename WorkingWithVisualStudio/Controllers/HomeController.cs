8ed545d5fe36c0930f33cc3adde46e4db743dd0e
Vasilisdm <vasilisdmr@gmail.com>
2019-06-02 19:11:52 +0300

    Created HomeController within controllers's folder.

---
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using WorkingWithVisualStudio.Models;
+
+namespace WorkingWithVisualStudio.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View(ProductRepository.SharedRepository.Products);
+      
+    }
+}
