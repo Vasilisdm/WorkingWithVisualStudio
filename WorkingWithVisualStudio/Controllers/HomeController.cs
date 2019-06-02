86a8f818204f9124a1528a15fae36a93fb68be41
Vasilisdm <vasilisdmr@gmail.com>
2019-06-02 20:23:25 +0300

    Filtered the products on price.

--- WorkingWithVisualStudio/Controllers/HomeController.cs
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using Microsoft.AspNetCore.Mvc;
 using WorkingWithVisualStudio.Models;
+using System.Linq;
 
 namespace WorkingWithVisualStudio.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View(ProductRepository.SharedRepository.Products);
+        public IActionResult Index() => View(ProductRepository.SharedRepository.Products
+                                            .Where(p => p.Price < 50));
       
     }
 }
