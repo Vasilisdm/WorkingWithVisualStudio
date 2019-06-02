ca231c400778d145662cdad7897a2a4978042c2c
Vasilisdm <vasilisdmr@gmail.com>
2019-06-02 21:34:53 +0300

    Added null value check.

--- WorkingWithVisualStudio/Controllers/HomeController.cs
+++ WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -8,7 +8,7 @@ namespace WorkingWithVisualStudio.Controllers
     public class HomeController : Controller
     {
         public IActionResult Index() => View(ProductRepository.SharedRepository.Products
-                                            .Where(p => p.Price < 50));
+                                            .Where(p => p?.Price < 50));
       
     }
 }
