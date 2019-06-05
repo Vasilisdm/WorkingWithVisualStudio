329234d76a45072d0c43f71ff9ea8132ad58196d
Vasilisdm <vasilisdmr@gmail.com>
2019-06-06 00:13:12 +0300

    Wrote 1 test for changing the name of the product, and one for changing the price.

---
+++ WorkingWithVisualStudio.Tests/ProductTests.cs
@@ -0,0 +1,34 @@
+﻿using WorkingWithVisualStudio.Models;
+using Xunit;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            // Arrange 
+            var p = new Product { Name = "Macbook pro", Price = 1438 };
+
+            // Act 
+            p.Name = "Macbook pro mid 2014";
+
+            // Assert 
+            Assert.Equal("Macbook pro mid 2014", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            // Arrange
+            var p = new Product { Name = "Mac pro", Price = 6000 };
+
+            // Act
+            p.Price = 6500;
+
+            // Assert
+            Assert.Equal(6500, p.Price);
+        }
+    }
+}
